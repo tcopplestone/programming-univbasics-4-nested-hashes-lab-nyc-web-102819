8802ed4604a68a3b773282659eb61fac34dfd8b3
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 14:20:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     (programmer_hash[:dennis_ritchie][:languages]).unshift("Assembly")
+     (programmer_hash[:dennis_ritchie][:languages]).push("Assembly")
      return programmer_hash
 end
