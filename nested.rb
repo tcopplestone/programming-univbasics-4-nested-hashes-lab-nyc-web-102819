e1a699a39e2e7fe322a9d334ed5382d48dd6253b
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 14:13:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
      programmer_hash[:alan_kay][:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     return programmer_hash
 end
 
 def adding_to_dennis
