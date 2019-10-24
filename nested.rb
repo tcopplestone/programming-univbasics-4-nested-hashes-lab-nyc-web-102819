73782411a5b71eed9411eccd745b46ac99919eb7
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-24 14:10:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-programmer_hash[yukihiro_matsumoto] = {
+     
+     programmer_hash[yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }
+return
 end
 
 def changing_alan
